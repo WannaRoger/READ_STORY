806bf3a1def331c1e89d0535c0b910f87ec49c75
quynhtranhuu <quynhthpn00114@gmail.com>
2024-11-23 08:08:17 +0700

    Create styles.php

--- frontend/truyen/truyen-moi-nhat.php
+++ frontend/truyen/truyen-moi-nhat.php
@@ -1,15 +1,3 @@
-<?php include_once(__DIR__ . '/../../../backend/dbconnect.php'); ?>
-<?php include_once(__DIR__ . '/../../../backend/custom_fomart.php'); ?>
-<!-- truyện mới -->
-<?php
-    $sql_truyen_moi = <<<EOT
-        SELECT truyen_id,truyen_ma,truyen_ten,truyen_tac_gia,truyen_mo_ta,truyen_anh_dai_dien,truyen_tinh_trang,
-        truyen_luot_xem,truyen_ngay_dang,truyen_trang_thai
-        FROM truyen 
-        WHERE truyen_trang_thai = "1"
-        ORDER BY truyen_id DESC
-        LIMIT 8
-    EOT;
     $result_truyen_moi = mysqli_query($conn, $sql_truyen_moi);
         $data_truyen_moi = [];
         while ($row = mysqli_fetch_array($result_truyen_moi, MYSQLI_ASSOC)) {
