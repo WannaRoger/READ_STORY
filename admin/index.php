2a5ec3903c2adf72d5fb110e2faef90fc1d223e0
Nguyễn Hữu Kiệt <kiet2k4dz@gmail.com>
2024-11-25 23:58:05 +0700

    Bổ sung sửa lỗi

--- admin/index.php
+++ admin/index.php
@@ -1,7 +1,9 @@
+<?php session_start()?>
 
-
-<link rel="stylesheet" href="../assets/css/admin-dashboard.css" type="text/css" />
-</head>
+<?php if(!isset($_SESSION['admin'])){
+		echo '<script> location.href="../auth/dang-nhap.php";</script>';
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -11,6 +13,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Truyện Cover</title>
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+
+    <!-- CSS dùng chung cho toàn bộ trang web -->
+    <?php include_once(__DIR__ . '/../frontend/layouts/admin-styles.php'); ?>
     <link rel="stylesheet" href="../assets/css/admin-dashboard.css" type="text/css" />
 </head>
 
@@ -19,23 +24,23 @@
     <?php include_once(__DIR__ . '/../backend/dbconnect.php'); ?>
 
     <!-- navigation -->
-    <?php include_once(__DIR__ . '/../frontend/partials/admin-sidebar.php'); ?>
+    <?php  include_once(__DIR__ . '/../frontend/partials/admin-sidebar.php'); ?>
 
     <!-- content -->
-    <?php include_once(__DIR__ . '/controller.php'); ?>
+    <?php  include_once(__DIR__ . '/controller.php'); ?>
 
     <!-- Nhúng file quản lý phần SCRIPT JAVASCRIPT -->
     <?php include_once(__DIR__ . '/../frontend/layouts/admin-scripts.php'); ?>
 
 </body>
-<?php
-if (isset($_GET['status']) && ($_GET['status'] == 'success')) {
-    echo '<script> toast.success("Thao tác thành công",500);</script>';
-}
-if (isset($_GET['status']) && ($_GET['status'] == 'error')) {
-    echo '<script> toast.error("Thao tác thành công",500);</script>';
-}
-
-?>
+<?php 
+    if(isset($_GET['status']) && ($_GET['status']=='success')){
+            echo '<script> toast.success("Thao tác thành công",500);</script>';
+        }
+    if(isset($_GET['status']) && ($_GET['status']=='error')){
+            echo '<script> toast.error("Thao tác thành công",500);</script>';
+        }
+    
+    ?>
 
 </html>
